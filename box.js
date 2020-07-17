438b5bae245a6d2c7dd1cd603b2335a247aec73a
Saanvi Gupta <65902729+Saanvi2828@users.noreply.github.com>
2020-07-17 15:45:47 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,18 @@
+ class Box{
+
+    constructor(x, y, width, height) {
+      var options = {
+          isStatic:true
+      }
+      this.body = Bodies.rectangle(x, y, width, height);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      rect(200,200,10,10);
+     // translate(this.body.position.x, this.body.position.y);
+      
+    }
+  }
+  
